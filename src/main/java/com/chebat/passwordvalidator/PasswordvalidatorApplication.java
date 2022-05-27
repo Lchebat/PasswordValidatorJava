a68481bddce41f3cd405980316a0a1a801aa412c
lchebat <lucaschebat@gmail.com>
2022-05-27 11:39:48 -0300

    Project started

---
+++ src/main/java/com/chebat/passwordvalidator/PasswordvalidatorApplication.java
@@ -0,0 +1,13 @@
+package com.chebat.passwordvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordvalidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordvalidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chebat/passwordvalidator/PasswordvalidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chebat.passwordvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordvalidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
